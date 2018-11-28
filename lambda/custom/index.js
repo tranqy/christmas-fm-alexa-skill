70ee7744475e6db80c290a5989a0fdc309903ede
Aaron Junod <tranqy@gmail.com>
2018-11-28 07:41:09 -0500

    Initial commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,183 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const STREAMS = [
+  {
+    "token": "merry-xmas",
+    "url": 'https://ice31.securenetsystems.net/XMASFMM',
+    "metadata" : {
+      "title": "Chirstmas FM",
+      "subtitle": "Bringing you the magic of Christmas",
+      "art": {
+        "sources": [
+          {
+            "contentDescription": "example image",
+            "url": "https://s3.amazonaws.com/cdn.dabblelab.com/img/audiostream-starter-512x512.png",
+            "widthPixels": 512,
+            "heightPixels": 512
+          }
+        ]
+      },
+      "backgroundImage": {
+        "sources": [
+          {
+            "contentDescription": "example image",
+            "url": "https://s3.amazonaws.com/cdn.dabblelab.com/img/wayfarer-on-beach-1200x800.png",
+            "widthPixels": 1200,
+            "heightPixels": 800
+          }
+        ]
+      }
+    }
+  }
+];
+
+const PlayStreamIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest' ||
+        (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'PlayStreamIntent') ||
+        (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.ResumeIntent');
+  },
+  handle(handlerInput) {
+
+    let stream = STREAMS[0];
+
+    handlerInput.responseBuilder
+      .speak(`starting ${stream.metadata.title}`)
+      .addAudioPlayerPlayDirective('REPLACE_ALL', stream.url, stream.token);
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'This skill just plays an audio stream when it is started. It does not have any additional functionality.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const AboutIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AboutIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'This is an audio starter template from skill templates dot com';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PauseIntent');
+  },
+  handle(handlerInput) {
+
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ALL')
+      .addAudioPlayerStopDirective();
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const PlaybackStoppedIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'AudioPlayer.PlaybackStopped';
+  },
+  handle(handlerInput) {
+    //should save details so play can be resumed. 
+    return true;
+  },
+};
+
+//AudioPlayer.PlaybackStarted
+const PlaybackStartedIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'AudioPlayer.PlaybackStarted';
+  },
+  handle(handlerInput) {
+    handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ENQUEUED');
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    console.log(handlerInput.requestEnvelope.request.error)
+
+    return handlerInput.responseBuilder
+      .getResponse();
+  },
+};
+
+//System.ExceptionEncountered
+const ExceptionEncounteredRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'System.ExceptionEncountered';
+  },
+  handle(handlerInput) {
+    console.log(`Errored Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    console.log(handlerInput)
+
+    return true;
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .addAudioPlayerClearQueueDirective('CLEAR_ALL')
+      .addAudioPlayerStopDirective()
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    PlayStreamIntentHandler,
+    PlaybackStartedIntentHandler,
+    CancelAndStopIntentHandler,
+    PlaybackStoppedIntentHandler,
+    AboutIntentHandler,
+    HelpIntentHandler,
+    ExceptionEncounteredRequestHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Christmas FM Alexa Skill
+
+As a long time Chirstmas FM listener not being able to listen on home Echo was not going to work. Christmas FM is available via TuneIn, but TuneIn for Alexa is not free. 
+
+This repository is a working, single purpose Alexa skill to stream Chirstmas FM while it's available. 
+
+I started with the Dabble streaming template (Dabble license file still included)
+
+https://dabblelab.com/
+
+I then grabbed the Chirstmas FM stream URL from here
+
+https://christmasfm.com/listenin/
+
+Changed the Stream URL in the lambda function, troubleshot for a minute until I realized we needed to use the https version, then had it streaming just in time to catch Christmas Shoes. 
+
+## Getting Started
+
+1) Clone this repository
+2) Install the ASK CLI from Amazon [Here](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html)
+3) Configure ASK [Instructions here](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html)
+3) Rename ./ask/config_template to ./ask/config
+4) Run ask deploy (I use the VS Code plugin from [here](https://marketplace.visualstudio.com/items?itemName=ask-toolkit.alexa-skills-kit-toolkit))
+5) Say "Alexa open my chirstmas fm"
+
+## Tips and donations
+Christmas FM is a charity station. Please direct all donations to Christmas FM's charity partner, Temple Street Children's University Hospital. 
+
+[Christmas FM Donation Page](https://www.templestreet.ie/Christmasfm/?utm_source=christmasfm.com&utm_medium=main_menu_link)
